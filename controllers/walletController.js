adef6df5513a2eb0d84afe27b944bac5dd6a2c47
sonukumar <sonu.kumar@antechindia.com>
2023-12-08 11:39:38 +0530

    some changes

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -4,14 +4,22 @@ const web3 = new Web3(rpcURL);
 
 const getWalletBalance = async (req, res) => {
   const address = "0x71C7656EC7ab88b098defB751B7401B5f6d8976F"; // Your account address goes here
-  web3.eth.getBalance(address, (err, wei) => {
-    if (err) {
-      console.log("error=", err);
-    } else {
-      console.log("balance=", wei);
-      res.status(200).json({ balance: wei });
-    }
-  });
+
+  // let k = await web3.eth.getAccounts();
+  // let k = await web3.eth.getGasPrice();
+  // web3.eth.defaultAccount = '0x11f4d0A3c12e86B4b5F39B213F7E19D048276DAe';
+  // let k = web3.eth.defaultAccount;
+  let k = web3.eth.defaultBlock;
+  console.log("okooooo==", k);
+
+  // web3.eth.getBalance(address, (err, wei) => {
+  //   if (err) {
+  //     console.log("error=", err);
+  //   } else {
+  //     console.log("balance=", wei);
+  //     res.status(200).json({ balance: wei });
+  //   }
+  // });
 };
 
 module.exports = {
